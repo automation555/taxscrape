b4d4fe0da4f69d5a368f15e173539912e536f938
Jamie Brough <jb@tempestattack.com>
2008-03-29 23:08:37 -0700

    quick clean up

--- tax.rb
+++ tax.rb
@@ -10,7 +10,7 @@ module Tax
       default_options = {}
       options = default_options.merge specified_options
       specified_options.keys.each do |key|
-        default_options.keys.include?(key) || raise(Chronic::InvalidArgumentException, "#{key} is not a valid option key.")
+        default_options.keys.include?(key) || raise(InvalidArgumentException, "#{key} is not a valid option key.")
       end
       @state = state
       @city = city
@@ -32,8 +32,8 @@ module Tax
         form = doc.form('_ctl0')
         form.fields.name('ListCity').options[1..-1].each do |it|
           begin #this needs debugging
-          p it.select
-          p @city = it.text #to be sure
+          it.select
+          @city = it.text #to be sure
           resp = Req.new(form, :submit).response
           data = resp.at('#lblCity')
           @rate = data.inner_text.match(/rate is(.*)/)[1].to_f
